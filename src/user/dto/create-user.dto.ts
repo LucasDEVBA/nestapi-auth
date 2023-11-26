1b06827c86f4857c6b55d3cbaf059913ca01e9ba
Lucas <lucasdevba@gmail.com>
2023-11-25 21:11:07 -0300

    Initial configs prisma module in user resources

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, MinLength, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  @MinLength(3)
+  @MaxLength(25)
+  email: string;
+
+  @IsString()
+  password: string;
+}
