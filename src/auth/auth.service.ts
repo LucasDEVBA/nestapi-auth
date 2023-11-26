59d5ac0df7a4bc2edd68eb7c7c1d9faca44c2c4a
Lucas <lucasdevba@gmail.com>
2023-11-25 23:54:57 -0300

    feat(auth): Implemented 'login' generating JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 import { UserService } from '../user/user.service';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/UserPayload';
+import { UserToken } from './models/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: User): UserToken {
+    // Transform user in to JWT
+    const payload: UserPayload = {
+      sub: user.id,
+      name: user.name,
+      email: user.email,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+    };
+  }
+
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
 
@@ -22,5 +45,4 @@ export class AuthService {
 
     throw new Error('Email address or password provided is incorrect.');
   }
-  // login() {}
 }
