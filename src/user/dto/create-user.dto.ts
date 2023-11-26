688f8b9e944dc763677232c1e7f3d801503bf13a
Lucas <lucasdevba@gmail.com>
2023-11-26 01:03:04 -0300

    feat(auth): implement login vlaidation

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
-import { IsString, IsEmail, MinLength, MaxLength } from 'class-validator';
+import {
+  IsString,
+  IsEmail,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -7,6 +13,9 @@ export class CreateUserDto {
   @IsEmail()
   @MinLength(3)
   @MaxLength(25)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
   email: string;
 
   @IsString()
